e9e8d0e7b33457639d0d1a3e52e87bb083d9a1b4
Tatiana Tomita <tatiana.tomita88@gmail.com>
2022-02-12 05:06:44 -0300

    cadastro leilao corrigido

--- src/test/java/br/com/alura/leilao/leiloes/CadastroLeiloesPage.java
+++ src/test/java/br/com/alura/leilao/leiloes/CadastroLeiloesPage.java
@@ -20,7 +20,7 @@ public class CadastroLeiloesPage {
 	public LeiloesPage cadastrarLeilao(String nome, String valorInicial, String dataAbertura) {
 		this.browser.findElement(By.id("nome")).sendKeys(nome);
 		this.browser.findElement(By.id("valorInicial")).sendKeys(valorInicial);
-		this.browser.findElement(By.id("daraAbertura")).sendKeys(dataAbertura);
+		this.browser.findElement(By.id("dataAbertura")).sendKeys(dataAbertura);
 		this.browser.findElement(By.id("button-submit")).submit();
 		
 		return new LeiloesPage(browser);
